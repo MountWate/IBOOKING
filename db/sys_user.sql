ab9dc9373008b3ed010bd525cc204c8de4665664
Yizhxy <1476672698@qq.com>
2025-05-28 19:55:14 +0800

    Create sys_user.sql

---
+++ db/sys_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO `sys_user` VALUES (1, 'admin', 'admin', '管理员', 'admin@qq.com', '13988888888', '复旦大学', '2023-04-14 13:36:46', 'http://localhost:8090/file/2355ecc3fed54895bcc29da5d9a2e090.png', 'ROLE_ADMIN', 0);
+INSERT INTO `sys_user` VALUES (3, 'syf', '123456', '苏永甫', '22210240271@m.fudan.edu.cn', '13301665238', '复旦大学计算机学院', '2023-06-08 20:10:01', 'http://49.235.115.169:8090/file/32cf0764916d4d339135546610a918cd.jpg', 'ROLE_ADMIN', 0);
+INSERT INTO `sys_user` VALUES (4, 'xwc', '123456', '谢万超', '15704912683@163.com', '12312312312', '复旦大学', '2023-03-15 15:56:16', '', 'ROLE_USER', 0);
+INSERT INTO `sys_user` VALUES (5, 'xuzheng', '123456', '徐铮', '22210240335@m.fudan.edu.cn', '13413413413', '复旦大学', '2023-01-10 15:56:56', NULL, 'ROLE_USER', 0);
+INSERT INTO `sys_user` VALUES (11, 'xym', '123456', '谢轶铭', '22210240326@m.fudan.edu.cn', '13124141341', '复旦大学计算机', '2023-08-17 17:21:42', NULL, 'ROLE_USER', 0);
+INSERT INTO `sys_user` VALUES (15, 'layout', '202cb962ac59075b964b07152d234b70', '鲁游', '1634201709@qq.com', '15380338027', '复旦大学', '2023-06-18 08:14:10', NULL, 'ROLE_USER', NULL);
